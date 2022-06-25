2f2e19c559b6212d1602964a23781e851e23c3d3
krishna kumar <krishnakumarjms12@gmail.com>
2022-06-25 13:17:42 +0530

    floyd cycle detection algo added

---
+++ 17LinkedList/20floydCycleDetectionAlgo.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+bool hasCycle(ListNode *head)
+{
+    ListNode *slow = head;
+    ListNode *fast = head;
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (fast == slow)
+        {
+            return true;
+        }
+    }
+    return false;
+}
