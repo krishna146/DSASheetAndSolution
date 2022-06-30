4333b5540f5c1fc2dd9cca65b5071e307def307d
krishna kumar <krishnakumarjms12@gmail.com>
2022-06-30 09:51:59 +0530

    valid paranthesis code added

--- 18Stack/7validParanthesis.cpp
+++ 18Stack/7validParanthesis.cpp
@@ -1,8 +1,55 @@
-//Problem Link - https://leetcode.com/problems/valid-parentheses/
+// Problem Link - https://leetcode.com/problems/valid-parentheses/
 /* By Krishna Kumar */
-#include<iostream>
-#include<stack>
+#include <iostream>
+#include <stack>
 using namespace std;
-int main(){
-    
+bool match(char close, char open)
+{
+    if (close == ')' && open == '(')
+        return true;
+    else if (close == ']' && open == '[')
+        return true;
+    else if (close == '}' && open == '{')
+        return true;
+    return false;
+}
+
+bool isValid(string s)
+{
+    if (s.length() & 1)
+        return false;
+    stack<char> st;
+    for (int i = 0; i < s.length(); i++)
+    {
+        char ch = s[i];
+        if (ch == '(' || ch == '[' || ch == '{')
+        {
+            st.push(ch);
+        }
+        else
+        {
+            if (!st.empty())
+            {
+                if (match(ch, st.top()))
+                {
+                    st.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+                return false;
+        }
+    }
+
+    if (st.empty())
+    {
+        return true;
+    }
+    return false;
+}
+int main()
+{
 }
