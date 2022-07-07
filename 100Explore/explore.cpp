0fef2280dc84c34a12a8e3e990869e80f44d1d23
krishna kumar <krishnakumarjms12@gmail.com>
2022-07-07 12:58:09 +0530

    sliding Window Maximum

--- 100Explore/explore.cpp
+++ 100Explore/explore.cpp
@@ -1,39 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
-void printVectorString(vector<string> &ump)
-{
-	for (int i = 0; i < ump.size(); i++)
-	{
-		for (int j = 0; j < ump[i].size(); j++)
-		{
-			cout << ump[i][j];
-		}
-		cout << endl;
-	}
-}
 int main()
 {
-	// string s(2, '.');
-	// s[0] = 'c';
-	// cout << s ;
-	// vector<string> ump(2, string(2, '.'));
-	// for(int i = 0 ; i < 2 ; i++){
-	// 	for(int j = 0 ; j < 2 ; j++){
-	// 		ump[i][j] = 'h';
-	// 	}
-	// 	cout << endl;
-	// }
-	// printVectorString(ump);
-	// for (int i = 0; i < 2 ; i++)
-	// {
-	// 	for(int j = 0 ; j < ump[i].size() ; j++){
-	// 		cout << ump[i][j];
-	// 	}
-	// 	cout << endl;
-	// }
-	string s = "krishna";
-	s.erase(s.begin() + 1);
-	cout << s << endl;
-	s.insert(s.begin() + 1, 'r');
-	cout << s << endl;
+	deque<int> dq(5);
+	while(!dq.empty()){
+		cout << dq.front() << " ";
+		dq.pop_front();
+	}
 }
---
+++ 19Queue/7slidingWindowMaximum.cpp
@@ -0,0 +1,38 @@
+// Problem Link - https://leetcode.com/problems/sliding-window-maximum/
+/* By Krishna Kumar */
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+class Solution
+{
+public:
+    vector<int> maxSlidingWindow(vector<int> &nums, int k)
+    {
+        deque<int> dq;
+        vector<int> ans;
+        for (int i = 0; i < k; i++)
+        {
+            while (!dq.empty() && nums[dq.back()] <= nums[i])
+            {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+        }
+        ans.push_back(nums[dq.front()]);
+        for (int i = k; i < nums.size(); i++)
+        {
+            if (!dq.empty() && (i - dq.front()) == k)
+                dq.pop_front();
+            while (!dq.empty() && nums[i] >= nums[dq.back()])
+            {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            ans.push_back(nums[dq.front()]);
+        }
+        return ans;
+    }
+};
+int main()
+{
+}
