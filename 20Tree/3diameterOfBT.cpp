3cdf749c5d8bc075a654fad2078895ed321bc82a
krishna kumar <krishnakumarjms12@gmail.com>
2022-07-09 20:10:34 +0530

    diameter of Bt code added

---
+++ 20Tree/3diameterOfBT.cpp
@@ -0,0 +1,41 @@
+// Problem Link -
+/* By Krishna Kumar */
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution
+{
+private:
+    int height(TreeNode *root)
+    {
+        if (root == NULL)
+        {
+            return 0;
+        }
+        return 1 + max(height(root->left), height(root->right));
+    }
+
+public:
+    int diameterOfBinaryTree(TreeNode *root)
+    {
+        if (root == NULL)
+            return 0;
+
+        int option1 = diameterOfBinaryTree(root->left);
+        int option2 = diameterOfBinaryTree(root->right);
+        int option3 = height(root->left) + height(root->right);
+        return max(option1, max(option2, option3));
+    }
+};
+int main()
+{
+}
