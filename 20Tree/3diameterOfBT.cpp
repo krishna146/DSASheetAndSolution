8cd43084eed44c33a648e4b75c47f93d911c35f7
krishna kumar <krishnakumarjms12@gmail.com>
2022-07-10 12:33:11 +0530

    diameter of binary tree optimized approach code added

--- 20Tree/3diameterOfBT.cpp
+++ 20Tree/3diameterOfBT.cpp
@@ -12,6 +12,7 @@ struct TreeNode
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
+//approach1: O(N^2)
 class Solution
 {
 private:
---
+++ 20Tree/4dimaterOptimized.cpp
@@ -0,0 +1,40 @@
+//Problem Link - 
+/* By Krishna Kumar */
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+//approach1: O(N)
+class Solution {
+private:
+   pair<int, int> diameter(TreeNode* root){
+        if(root == NULL){
+           return {0, 0};
+        }
+        pair<int, int> option1 = diameter(root -> left);
+        pair<int, int> option2= diameter(root -> right);
+        int leftHeight = option1.second;
+        int rightHeight = option2.second;
+        int option3 = leftHeight + rightHeight;       
+        int currNodeHeight = 1 + max(leftHeight, rightHeight);
+        return {max(option1.first, max(option2.first, option3)), currNodeHeight};
+    }
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        if(root == NULL) return 0;
+        pair<int,int> ans = diameter(root);
+        return ans.first;
+        
+    }
+};
+int main(){
+    
+}
