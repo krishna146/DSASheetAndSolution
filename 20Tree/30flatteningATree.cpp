64a5d8c2f15bfa85700673737c2ebb75a69574ea
krishna kumar <krishnakumarjms12@gmail.com>
2022-07-16 11:06:14 +0530

    min depth of binary tree code added

--- 20Tree/30flatteningATree.cpp
+++ 20Tree/30flatteningATree.cpp
@@ -105,6 +105,26 @@ public:
         }
     }
 };
-int main()
+//Approach3: Morris Traversal -> TC = O(N) SC = O(1)
+class Approach3
 {
-}
+    public:
+    void flatten(Node *root)
+    {
+        Node* curr = root;
+        while(curr){
+            if(curr -> left ){
+                Node* pred = curr -> left;
+                while(pred -> right){
+                    pred = pred -> right;
+                }
+                pred -> right = curr -> right;
+                curr -> right = curr -> left;
+                curr -> left = NULL;
+                
+            }
+            curr = curr -> right;
+        }
+        
+    }
+};
---
+++ 20Tree/32minmTree.cpp
@@ -0,0 +1,27 @@
+//Problem Link - https://leetcode.com/problems/minimum-depth-of-binary-tree/
+/* By Krishna Kumar */
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(root == NULL) return 0;
+        if(root -> left == NULL && root -> right == NULL) return 1;
+        if(root -> left == NULL)
+            return 1 + minDepth(root -> right);
+        else if(root -> right == NULL)
+            return 1 + minDepth(root -> left);
+        else
+            return 1 + min(minDepth(root-> left), minDepth(root -> right));
+    }
+};
