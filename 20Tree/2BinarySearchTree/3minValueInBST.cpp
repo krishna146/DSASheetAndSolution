666597d0cb81d1923d2f222d11dcf23ccc207504
krishna kumar <krishnakumarjms12@gmail.com>
2022-07-17 11:01:57 +0530

    find minimum value in BST code added

---
+++ 20Tree/2BinarySearchTree/3minValueInBST.cpp
@@ -0,0 +1,23 @@
+// Problem Link -
+/* By Krishna Kumar */
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+int minValue(Node *root)
+{
+    // Code here
+    if (!root)
+        return -1;
+    if (!root->left)
+        return root->data;
+    return minValue(root->left);
+}
+int main()
+{
+}
