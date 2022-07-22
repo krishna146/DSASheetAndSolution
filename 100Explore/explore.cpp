b72d924778aa8b512a9a1bde2a28edb80c840eb5
krishna kumar <krishnakumarjms12@gmail.com>
2022-07-22 10:05:24 +0530

    Binary tree assignment pdf added

--- 100Explore/explore.cpp
+++ 100Explore/explore.cpp
@@ -1,8 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
+class Solution
+{
+public:
+    string solve(int N, vector<int> &A)
+    {
+        // code here
+        string yes = "YES";
+        string no = "NO";
+        int first = A[0];
+        int second = A[1];
+        if (first == -1 * second)
+        {
+            for (int i = 2; i < A.size(); i++)
+            {
+                if (i & 1 == 0 && A[i] != first)
+                {
+                    return no;
+                }
+                if (i & 1 && A[i] != second)
+                {
+                    return no;
+                }
+            }
+            return yes;
+        }
+        return no;
+    }
+};
 int main()
 {
-    unordered_map<int, bool> ump;
-    cout << ump[1] << endl;
-    cout << ump[1] << endl;
+   int j = 1;
+   int num1 = 4;
+   while(++j <= 10){
+    num1++;
+   }
+   cout << num1 << endl;
+   
 }
