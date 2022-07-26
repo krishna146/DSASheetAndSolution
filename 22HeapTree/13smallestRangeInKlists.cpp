b485bbe238d9f460a2fbe17d8c043dc0b6884b7f
krishna kumar <krishnakumarjms12@gmail.com>
2022-07-27 01:05:01 +0530

    Smallest Range Covering Elements from K Lists

---
+++ 22HeapTree/13smallestRangeInKlists.cpp
@@ -0,0 +1,71 @@
+// Problem Link - https://leetcode.com/problems/smallest-range-covering-elements-from-k-lists/
+/* By Krishna Kumar */
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+class Info
+{
+public:
+    int data;
+    int row;
+    int col;
+    Info(int d, int r, int c)
+    {
+        this->data = d;
+        this->row = r;
+        this->col = c;
+    }
+};
+class Cmp
+{
+public:
+    bool operator()(Info *a, Info *b)
+    {
+        return a->data > b->data;
+    }
+};
+class Solution
+{
+public:
+    vector<int> smallestRange(vector<vector<int>> &nums)
+    {
+        int maxi = INT_MIN;
+        int mini = INT_MAX;
+        priority_queue<Info *, vector<Info *>, Cmp> minHeap;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int el = nums[i][0];
+            Info *info = new Info(el, i, 0);
+            maxi = max(maxi, el);
+            mini = max(mini, el);
+            minHeap.push(info);
+        }
+        int start = maxi;
+        int end = mini;
+        while (!minHeap.empty())
+        {
+            Info *temp = minHeap.top();
+            minHeap.pop();
+            mini = temp->data;
+            if (maxi - mini < end - start)
+            {
+                start = mini;
+                end = maxi;
+            }
+            if (temp->col + 1 < nums[temp->row].size())
+            {
+                maxi = max(maxi, nums[temp->row][temp->col + 1]);
+                Info *toPush = new Info(nums[temp->row][temp->col + 1], temp->row, temp->col + 1);
+                minHeap.push(toPush);
+            }
+            else
+            {
+                break;
+            }
+        }
+        vector<int> ans;
+        ans.push_back(start);
+        ans.push_back(end);
+        return ans;
+    }
+};
