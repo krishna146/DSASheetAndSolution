9db6fc31e02d8f73729860de43cb9a28828deaa9
krishna kumar <krishnakumarjms12@gmail.com>
2022-07-29 13:33:44 +0530

    Convert Sorted Array to Binary Search Tree

--- 21BinarySearchTree/HomeWork/21sortedArrayToBST.cpp
+++ 21BinarySearchTree/HomeWork/21sortedArrayToBST.cpp
@@ -1,22 +1,35 @@
-//Problem Link - https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+// Problem Link - https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
 /* By Krishna Kumar */
-#include<bits/stdc++.h>
-#include<iostream>
+#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-class Solution {
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution
+{
 private:
-    TreeNode* solve(vector<int>& nums, int left , int right){
-        if(left > right) return NULL; 
+    TreeNode *solve(vector<int> &nums, int left, int right)
+    {
+        if (left > right)
+            return NULL;
         int mid = left + (right - left) / 2;
         int data = nums[mid];
-        TreeNode* root = new TreeNode(data);
-        root -> left = solve(nums, left, mid - 1);
-        root -> right = solve(nums, mid + 1, right);
+        TreeNode *root = new TreeNode(data);
+        root->left = solve(nums, left, mid - 1);
+        root->right = solve(nums, mid + 1, right);
         return root;
-        
     }
+
 public:
-    TreeNode* sortedArrayToBST(vector<int>& nums) {
+    TreeNode *sortedArrayToBST(vector<int> &nums)
+    {
         return solve(nums, 0, nums.size() - 1);
     }
 };
