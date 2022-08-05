31f09393c23eee75ffa23395f55200744b5e64aa
krishna kumar <krishnakumarjms12@gmail.com>
2022-08-05 16:07:05 +0530

    postOrder iterative code added

--- 20BinaryTree/HomeWork/postOrderIterative.cpp
+++ 20BinaryTree/HomeWork/postOrderIterative.cpp
@@ -1,4 +1,4 @@
-// Problem Link - 
+// Problem Link - https://practice.geeksforgeeks.org/problems/postorder-traversal-iterative/1?page=1&curated[]=6&sortBy=accuracy
 /* By Krishna Kumar */
 #include <bits/stdc++.h>
 #include <iostream>
@@ -8,4 +8,25 @@ struct Node
     int data;
     Node *left;
     Node *right;
+};
+class Solution{
+    public:
+    vector<int> postOrder(Node* root) {
+        vector<int> ans;
+        stack<Node *> st;
+        while (!st.empty() || root)
+        {
+            while (root)
+            {
+                ans.push_back(root -> data);
+                st.push(root);
+                root = root->right;
+            }
+            Node *top = st.top();
+            st.pop();
+            root = top-> left;
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
 };
