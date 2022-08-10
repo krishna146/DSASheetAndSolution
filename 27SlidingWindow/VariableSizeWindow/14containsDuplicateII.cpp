fe5438981cc366ef4f116195fa1f56e707b35225
krishna kumar <krishnakumarjms12@gmail.com>
2022-08-10 17:20:15 +0530

    Containd duplicate question added

---
+++ 27SlidingWindow/VariableSizeWindow/14containsDuplicateII.cpp
@@ -0,0 +1,31 @@
+// Problem Link - https://leetcode.com/problems/contains-duplicate-ii/
+/* By Krishna Kumar */
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+class Solution
+{
+public:
+    bool containsNearbyDuplicate(vector<int> &nums, int k)
+    {
+        unordered_map<int, bool> ump;
+        int left = 0;
+        int right = 0;
+        for (int right = 0; right < nums.size();)
+        {
+            if (right - left > k)
+            {
+                ump[nums[left++]] = false;
+            }
+            else
+            {
+                if (ump[nums[right]])
+                {
+                    return true;
+                }
+                ump[nums[right++]] = true;
+            }
+        }
+        return false;
+    }
+};
